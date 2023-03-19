07dc4943aa03d067a5360baa9e87b722ee584773
riosp <riosp9932@gmail.com>
2023-03-19 19:47:19 +0900

    try

--- src/BeakJoon_Reapeating/Bj25314.java
+++ src/BeakJoon_Reapeating/Bj25314.java
@@ -1,4 +1,16 @@
 package BeakJoon_Reapeating;
 
+import java.io.*;
+
 public class Bj25314 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+
+    }
 }
