99c39864c539b65b9974e8546a901abcdcdfd21f
Riosp <riosp9932@gmail.com>
2023-03-20 17:13:24 +0900

    230320S

--- src/BeakJoon_Array/Bj10810.java
+++ src/BeakJoon_Array/Bj10810.java
@@ -11,10 +11,27 @@ public class Bj10810 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer stt;
-        stt = new StringTokenizer(br.readLine(), " ");
-        int linelen = Integer.parseInt(stt.nextToken());
-        int caseNum = Integer.parseInt(stt.nextToken());
+        String str = br.readLine();
+        String[] strs = str.split(" ");
+        int linelen = Integer.parseInt(strs[0]);
+        int caseNum = Integer.parseInt(strs[1]);
+        int[] line = new int [linelen];
+        for(int i=0 ; i<linelen ; i++){
+            line[i] = 0;
+        }
+        for (int i=0 ; i<caseNum ; i++){
+            stt = new StringTokenizer(br.readLine(), " ");
+            int first = Integer.parseInt(stt.nextToken());
+            int second = Integer.parseInt(stt.nextToken());
+            int putNum = Integer.parseInt(stt.nextToken());
+            for(int j=first+1 ; j<=second ; j++){
+                line[j] = putNum;
+            }
+        }
 
-
+        for(int i=0 ; i<linelen ; i++){
+            bw.write(String.valueOf(line[i])+" ");
+        }
+        bw.flush();
     }
 }
