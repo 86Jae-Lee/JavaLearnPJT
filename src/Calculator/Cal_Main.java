a583e21a386d6d46e6b907dd6c008383af78a4ab
riosp <riosp9932@gmail.com>
2023-05-21 23:28:49 +0900

    230521F

---
+++ src/Calculator/Cal_Main.java
@@ -0,0 +1,27 @@
+package Calculator;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Cal_Main {
+
+    static int num1;
+    static int num2;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("계산기를 시작합니다. ");
+
+
+    }
+    static int plus(int a, int b){
+        return a+b;
+    }
+    static int minus(int a, int b){
+        return a-b;
+    }
+    static int
+
+}
