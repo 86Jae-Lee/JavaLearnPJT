dc40c9102ad70be8e82a2b9b450c9ea278dfec79
Riosp <riosp9932@gmail.com>
2023-05-26 15:25:48 +0900

    230526F

--- src/Calculator/Cal_Main.java
+++ src/Calculator/Cal_Main.java
@@ -10,9 +10,13 @@ public class Cal_Main {
     static int num2;
 
     public static void main(String[] args) throws IOException {
-
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write("계산기를 시작합니다. ");
+        bw.flush();
+        num1 = 0;
+        while(true){
+
+        }
 
 
     }
@@ -22,6 +26,9 @@ public class Cal_Main {
     static int minus(int a, int b){
         return a-b;
     }
-    static int
+    static int multiple(int a, int b) { return a*b; }
+    static double divide(int a, int b) { return a/b; }
+
+
 
 }
